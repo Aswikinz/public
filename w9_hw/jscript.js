333ef447301136e859718d4430bd08b0ef398835
Aswikinz <113660766+Aswikinz@users.noreply.github.com>
2022-09-20 16:14:48 +1000

    week9 Homework uploaded

---
+++ w9_hw/jscript.js
@@ -0,0 +1,3 @@
+var vg_1 = "vegalite.json"; vegaEmbed("#map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
